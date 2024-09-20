af8e9aa81f8b84e90129f3349262ed0d428533d4
Muhammad Farhan <evirsanti@gmail.com>
2024-09-21 05:45:06 +0800

    feat: add error page

---
+++ src/routes/error.tsx
@@ -0,0 +1,10 @@
+
+
+export default function ErrorPage() {
+    return (
+        <>
+            <h1>Oops Error detection!!</h1>
+            <p>This page is not already</p>
+        </>
+    );
+};
