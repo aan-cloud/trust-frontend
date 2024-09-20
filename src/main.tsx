1141865f71f3bd4577c9c6253019b97aab55b1dd
Muhammad Farhan <evirsanti@gmail.com>
2024-09-21 05:48:45 +0800

    feat: add loader for home and products

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,68 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import ErrorPage from './routes/error'
+
+import RootRoute, { rootLoader  } from './routes/root'
+
+import HomeRoute, {homeLoader} from './routes/home'
+
+import ProductsRoute from './routes/products'
+
+const router = createBrowserRouter([
+  {
+    id: "root",
+    path: "/",
+    element: <RootRoute />,
+    //errorElement: <ErrorPage />,
+    //loader: rootLoader,
+    children: [
+      {
+        path: "/",
+        element: <HomeRoute />,
+        loader: homeLoader
+      },
+      {
+        path: "/:products",
+        element: <ProductsRoute />
+       // loader: productsLoader
+      },
+      // {
+      //   path: "/products/:slug",
+      //   element: <ProductSlugRoute />,
+      //   loader: productSlugLoader,
+      //   action: productSlugAction,
+      // },
+      // {
+      //   path: "/register",
+      //   element: <RegisterRoute />,
+      //   loader: registerLoader,
+      //   action: registerAction,
+      // },
+      // {
+      //   path: "/login",
+      //   element: <LoginRoute />,
+      //   loader: loginLoader,
+      //   action: loginAction,
+      // },
+      // {
+      //   path: "/dashboard",
+      //   element: <UserDashboardRoute />,
+      //   loader: userDashboardLoader,
+      //   action: userDashboardAction,
+      // },
+      // {
+      //   path: "/cart",
+      //   element: <CartRoute />,
+      //   loader: cartLoader,
+      // },
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
