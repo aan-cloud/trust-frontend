a6eb48103a9e82148b78658d7a8f7856abf4e024
Muhammad Farhan <evirsanti@gmail.com>
2024-09-22 02:44:11 +0800

    feat: add root loader and route component

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,43 +1,27 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Navigation from "../layouts/navigation";
+import Products from "../layouts/productsGrid";
 
+export async function rootLoader() {
+  try {
+    const response = await fetch("https://trust-backend-jvcy.onrender.com");
 
-export async function rootLoader () {
-    try {
-        const response = await fetch('https://trust-backend-jvcy.onrender.com');
-
-        if (!response.ok) {
-            throw new Error(`response status: ${response.status}`)
-        };
-
-        const data = await response.json();
-        return data
-    } catch (error: any | unknown) {
-        return `Error ${error.message}`
+    if (!response.ok) {
+      throw new Error(`response status: ${response.status}`);
     }
-};
 
-export default function RootRoute () {
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to={'/'}>Home</Link>
-                    </li>
-                    <li>
-                        <Link to={'/login'}>Login</Link>
-                    </li>
-                    <li>
-                        <Link to={'/register'}>Register</Link>
-                    </li>
-                    <li>
-                        <Link to={'/products'}>Products</Link>
-                    </li>
-                </ul>
-            </nav>
-            <div>
-                <Outlet />
-            </div>
-        </div>
-    );
-};
+    const data = await response.json();
+    return data;
+  } catch (error: any | unknown) {
+    return `Error ${error.message}`;
+  }
+}
+
+export default function RootRoute() {
+  return (
+    <div className="">
+      <Navigation />
+      <Outlet />
+    </div>
+  );
+}
