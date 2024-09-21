2cd4eeeb26141441ab28bd60dbdd2342837a06ec
Muhammad Farhan <evirsanti@gmail.com>
2024-09-22 02:41:18 +0800

    feat: add logo component

---
+++ src/components/logo.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Logo() {
+  return (
+    <Link to="/">
+      <img
+        src="https://ucarecdn.com/70003910-bdb5-4713-87a3-6366201bb029//"
+        className="h-9 w-40 mr-[350px]"
+      />
+    </Link>
+  );
+}
