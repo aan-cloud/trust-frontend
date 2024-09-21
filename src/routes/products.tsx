659bb8a85e53bcb9f0135a1251fed4d326cc1781
Muhammad Farhan <evirsanti@gmail.com>
2024-09-22 02:42:46 +0800

    feat: add products loader and route component

--- src/routes/products.tsx
+++ src/routes/products.tsx
@@ -1,7 +1,30 @@
+import { useLoaderData, LoaderFunctionArgs } from "react-router-dom";
+import products from "../types/productCard";
+import Products from "../layouts/productsGrid";
 
-
-export default function ProductsRoute () {
-    return (
-        <h1>Ini adalah halaman products</h1>
+export async function productsLoader({ params }: LoaderFunctionArgs) {
+  try {
+    const response = await fetch(
+      `https://trust-backend-jvcy.onrender.com/products/${params.products}`,
     );
-};
+
+    if (!response.ok) {
+      throw new Error(`Error with code: ${response.status}`);
+    }
+
+    const products = await response.json();
+    return { products };
+  } catch (error: any | unknown) {
+    return { products: null };
+  }
+}
+
+export default function ProductsRoute() {
+  const { products } = useLoaderData() as { products: products };
+
+  return (
+    <div>
+      <Products products={products} />
+    </div>
+  );
+}
