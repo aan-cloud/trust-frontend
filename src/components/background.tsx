cce5760ab94f3851c7e265053270f89c6fb0547e
Muhammad Farhan <evirsanti@gmail.com>
2024-09-22 20:10:11 +0800

    feat: add animatin background

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,15 +1,14 @@
 export default function Background () {
     return (
-        <div className="relative bg-[url('https://ucarecdn.com/6d7116af-a08a-445f-85d4-6d9fca65a6bb/')] bg-cover bg-no-repeat bg-center h-[calc(100vh-160px)] w-full flex flex-col items-center justify-center">
-            <div className="absolute inset-0 bg-black opacity-50"></div>
-            <div className="relative z-10 text-white p-4">
-            <h1 className="text-6xl text-center font-bold font-poppins text-white mb-3">
-                TRUST
-            </h1>
-            <h3 className="text-2xl font-semibold font-poppins text-white">
-                Buy spare parts from trusted manufacturers
-            </h3>
-            </div>
-        </div>
+       <div className="relative h-screen w-full">
+  <video autoPlay loop muted playsInline className="absolute -z-10 top-0 left-0 h-full w-full object-cover">
+    <source src="https://ucarecdn.com/3f99659b-2888-40f0-85b0-af727fe6c1aa/" type="video/mp4" />
+    Your browser does not support the video tag.
+  </video>
+  <div className="relative z-10 flex items-start flex-col justify-end p-20 h-full">
+    <h1 className="text-white text-8xl font-medium font-roboto tracking-widest">Trust<sup>&copy;</sup></h1>
+    <p className="text-white text-8xl font-medium font-poppins">Just believe it</p>
+  </div>
+</div>
     )
 }
