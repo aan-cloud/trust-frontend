67bbc21ca50fd9cd73ff476af61904b1588a6dcc
Muhammad Farhan <evirsanti@gmail.com>
2024-09-23 23:26:49 +0800

    refactor: change background homepage

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,13 +1,13 @@
+import { Link } from "react-router-dom"
+
 export default function Background () {
     return (
-       <div className="relative h-screen w-full">
-  <video autoPlay loop muted playsInline className="absolute -z-10 top-0 left-0 h-full w-full object-cover">
-    <source src="https://ucarecdn.com/3f99659b-2888-40f0-85b0-af727fe6c1aa/" type="video/mp4" />
-    Your browser does not support the video tag.
-  </video>
-  <div className="relative z-10 flex items-start flex-col justify-end p-20 h-full">
-    <h1 className="text-white text-8xl font-medium font-roboto tracking-widest">Trust<sup>&copy;</sup></h1>
-    <p className="text-white text-8xl font-medium font-poppins">Just believe it</p>
+       <div className="relative w-full bg-[url('https://ucarecdn.com/05bda61e-6da6-4e16-9d88-cbc57fd0de94/')] bg-cover bg-center h-screen')] h-screen">
+  <div className="relative z-10 flex items-center p-20 flex-col justify-center h-full">
+    <p className="text-[#B4B4B4] text-7xl font-bold font-poppins mb-3">Just believe <span className="underline">trust</span></p>
+    <button className="w-[133px] h-[42px] text-[#EEEEF0] font-poppins text-base font-normal tracking-wide backdrop-blur-md rounded-lg border border-[#B4B4B4]">
+      <Link to={'/products'} className="w-full h-full flex items-center justify-center">Explore</Link>
+    </button>
   </div>
 </div>
     )
