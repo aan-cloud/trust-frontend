15caac4dd6cf952ad5797eea86dd7b6e383f0b9f
Muhammad Farhan <evirsanti@gmail.com>
2024-09-23 23:27:10 +0800

    feat:add button component

---
+++ src/components/button.tsx
@@ -0,0 +1,5 @@
+export default function Button ({value}: any) {
+    return (
+        <button className="py-1 rounded-sm font-light text-sm px-1 text-[hsla(0deg,0%,98%,60%)]">{value}</button>
+    )
+}
