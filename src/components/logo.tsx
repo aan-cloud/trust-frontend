ff9f2169f185ea3c280a49d2de1e6a3e7c5057b0
Muhammad Farhan <evirsanti@gmail.com>
2024-09-26 19:48:15 +0800

    refactor: fixed widest logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,7 +6,7 @@ export default function Logo() {
       <Link to="/" className="w-fit block">
         <img
           src="https://ucarecdn.com/714833c2-e319-4aa8-b734-ec9ef74e9bc5/"
-          className="h-7 w-28"
+          className="h-7 min-w-24"
         />
       </Link>
     </div>
