a477fdb02232b559d8955fd57000f278d055deb0
Farhan <evirsanti@gmail.com>
2024-10-04 13:02:10 +0800

    feat: add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer id="footer" className="w-full bg-[rgba(41,41,41,0.4)] text-white px-4 py-7 transition-transform duration-300 transform translate-y-0">
+            <p className="text-center font-poppins ">2024&copy; <b>Trust</b>. All reserved </p>
+        </footer>
+    )
+}
