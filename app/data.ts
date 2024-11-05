0c3a2312f27cdc6b75be7b354872b4020e146dd1
Farhan <evirsanti@gmail.com>
2024-11-06 02:23:04 +0800

    feat: rewrite using remix

---
+++ app/data.ts
@@ -0,0 +1,16 @@
+/**
+ * This is just a data faker
+ */
+interface Option {
+    name: string
+}
+
+
+export const optionMenu: Option[] = [
+    { name: "Body parts"},
+    { name: "Electronic parts"},
+    { name: "Engine parts"},
+    { name: "Brake parts"},
+    { name: "Suspension parts"},
+    { name: "Exterior parts"}
+];
--- app/root.tsx
+++ app/root.tsx
@@ -2,10 +2,13 @@ import {
   Links,
   Meta,
   Outlet,
+  Form,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
+
+import { optionMenu } from "./data";
 
 import "./tailwind.css";
 
@@ -20,6 +23,27 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/apple-touch-icon.png"
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon-32x32.png"
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon-16x16.png"
+  },
+  {
+    rel:"manifest",
+    href: "/site.webmanifest"
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -32,6 +56,36 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <nav className="flex justify-between content-center">
+          <div id="logo">
+            <img src="https://ucarecdn.com/ae731f35-d732-4b7e-827d-72d4cace50a7/logotrustnewremovebgpreview.png"/>
+          </div>
+          <div className="flex justify-between" id="search">
+            <select name="" id="">
+              {
+                optionMenu.map((opt) => (
+                  <option value={opt.name}>{opt.name}</option>
+                ))
+              }
+            </select>
+            <Form method="post" role="search" id="search-from">
+              <input 
+              id="q"
+              aria-label="Search contacts"
+              //defaultValue={ q || "" }
+              placeholder="Search"
+              type="search"
+              //className={searching ? "loading" : ""}
+              name="q"/>
+              <button type="submit">Search</button>
+            </Form>
+          </div>
+          <div className="flex justify-between" id="feat">
+            <div>Wishlist</div>
+            <div>Login</div>
+            <div>Cart</div>
+          </div>
+        </nav>
         {children}
         <ScrollRestoration />
         <Scripts />
