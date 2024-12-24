7643610618b92a2e05a6d9dcd6b44dfd6756b15d
aan-cloud <evirsanti@gmail.com>
2024-12-24 17:11:33 +0800

    init: setup vite using template react router v7

---
+++ app/welcome/welcome.tsx
@@ -0,0 +1,10 @@
+import logoDark from "./logo-dark.svg";
+import logoLight from "./logo-light.svg";
+
+export function Welcome() {
+  return (
+    <div>
+      <h1 className="font-thin text-lg">Hi nama ku farhan</h1>
+    </div>
+  );
+}
