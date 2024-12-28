e598998e2f83305d3e21352388712ecddb2b479d
aan-cloud <evirsanti@gmail.com>
2024-12-28 23:13:24 +0800

    chore: register schema

---
+++ src/schema/register.ts
@@ -0,0 +1,8 @@
+"use client"
+import { z } from "zod";
+
+export const registerSchema = z.object({
+    userName: z.string().min(4, "Username must be contain at least 4 characters"),
+    email: z.string().email("Input the correct email"),
+    password: z.string().min(5, "PAssword contain at least 5 characters")
+});
