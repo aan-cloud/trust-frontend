ae9158275394a4cfcc77c1dc2e50a46499e35166
aan-cloud <evirsanti@gmail.com>
2025-01-03 06:09:57 +0800

    refactor: data schema

--- src/schema/data.ts
+++ src/schema/data.ts
@@ -1,9 +1,18 @@
 import { z } from "zod";
 
-export const dummySchema = z.object({
+const imageSchema = z.object({
+  id:z.string(),
+  imageUrl:z.string(),
+  productId:z.string(),
+});
+
+export const dataSchema = z.object({
   id: z.number(),
-  imageUrl: z.string(),
+  description: z.string(),
+  imageUrl: z.array(imageSchema),
   name: z.string(),
   price: z.number(),
+  stock: z.number(),
+  slug: z.string(),
   rate: z.number(),
 });
