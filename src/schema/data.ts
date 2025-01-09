0474b3601cfb652b809906165daaf1369cb7ac14
aan-cloud <evirsanti@gmail.com>
2025-01-09 14:37:03 +0800

    chore: schemas

--- src/schema/data.ts
+++ src/schema/data.ts
@@ -16,3 +16,35 @@ export const dataSchema = z.object({
   slug: z.string(),
   rate: z.number(),
 });
+
+const imageUrls = z.object({
+  id: z.string(),
+  imageUrl: z.string(),
+  productId: z.string(),
+});
+
+export const productDetailsSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  imageUrl: z.array(imageUrls),
+  price: z.number(),
+  slug: z.string(),
+  stock: z.number(),
+});
+
+export const breadcrumbSchema = z.object({
+  label: z.string(),
+  href: z.string(),
+  isCurrentPage: z.boolean(),
+});
+
+export const filterSchema = z.object({
+  price: z.number(),
+  category: z.string(),
+});
+
+export const inserToCartSchema = z.object({
+  quantity: z.number(),
+  productId: z.string(),
+});
