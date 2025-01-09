c29697e3879eab319aff3cd27a556a9a6302e93d
aan-cloud <evirsanti@gmail.com>
2025-01-09 14:34:23 +0800

    feat: footer ui component

--- src/components/pages/footer.tsx
+++ src/components/pages/footer.tsx
@@ -1,13 +1,13 @@
 export const Footer = () => {
   return (
-    <section className="flex justify-between border-t mt-4 px-24 py-14 gap-5 ">
+    <section className="flex justify-between px-24 py-14 gap-5 bg-popover-foreground">
       <div className="">
         <header>
           <h1 className="font-sans text-4xl font-black uppercase text-primary">
             trust.
           </h1>
         </header>
-        <small className="text-[12px]">
+        <small className="text-[12px] text-stone-400">
           Where Reliability Meets Excellence, Delivering Trust and Quality You
           Can Depend On.
         </small>
@@ -17,7 +17,9 @@ export const Footer = () => {
       </div>
       {Array.from({ length: 3 }, (_, index) => index++).map((i) => (
         <ul key={i}>
-          <li className="mb-4 text-lxl font-bold font-sans">Contoh</li>
+          <li className="mb-4 text-lxl font-bold font-sans text-stone-400">
+            Contoh
+          </li>
           <li className="font-light font-poppins text-sm text-stone-500">
             Contoh list
           </li>
