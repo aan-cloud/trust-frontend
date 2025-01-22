bce483eb9f6c6dc260162b022d47a2cacecfffc3
aan-cloud <evirsanti@gmail.com>
2025-01-22 12:21:05 +0800

    feat: success page

---
+++ src/app/success/page.tsx
@@ -0,0 +1,7 @@
+export default function Success () {
+    return (
+        <div className="h-screen px-24 py-5 flex justify-center items-center">
+            <h1 className="text-3xl text-blue-500 font-black">Your payment hasbeen Successfully</h1>
+        </div>
+    )
+}
