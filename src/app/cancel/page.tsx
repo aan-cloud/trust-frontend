310bec4ab1a0544c3ad74c597ae93d87e2259214
aan-cloud <evirsanti@gmail.com>
2025-01-22 12:20:53 +0800

    feat: cancel page

---
+++ src/app/cancel/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Cancel () {
+    return (
+        <div className="h-screen px-24 py-5 flex justify-center items-center">
+            <h1 className="text-3xl text-red-600 font-black">Your payment hasbeen Canceled</h1>
+        </div>
+    )
+}
