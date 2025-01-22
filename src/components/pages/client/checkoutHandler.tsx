c19abc76290e2eca280382bb95a6aa48fbc42502
aan-cloud <evirsanti@gmail.com>
2025-01-22 12:21:16 +0800

    feat: checkout handler

---
+++ src/components/pages/client/checkoutHandler.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { checkout } from "@/requests/checkout";
+import Cookies from "js-cookie";
+import { toast } from "sonner";
+
+export const CheckoutHandler = ({totalPrice}: {totalPrice: number}) => {
+    const router = useRouter();
+    const accesToken = Cookies.get("accesToken");
+
+    async function handleCheckout() {
+        if (totalPrice <= 0) {
+            toast('Checkout failed, add product to cart first!', {
+                description: new Date().toISOString().split('T')[0],
+                action: { label: 'Close', onClick: () => '' },
+            });
+            router.push("/products")
+        }
+        const redirectUrl = await checkout(accesToken);
+        router.push(redirectUrl);
+    }
+
+    return <Button onClick={() => handleCheckout()} className="mt-2">Checkout</Button>
+}
