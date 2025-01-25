e186b9e9da606d8d9b8249345b6d572aac494e97
aan-cloud <evirsanti@gmail.com>
2025-01-26 07:32:35 +0800

    feat: profile mobile responsive

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -2,7 +2,7 @@ import { UserProfile } from "@/components/pages/server/profile"
 
 export default function User () {
     return (
-        <div className="px-24 py-5 bg-white flex flex-col gap-3 pt-5 w-full">
+        <div className="sm:px-24 px-4 py-5 bg-white flex flex-col gap-3 pt-5 w-full">
             <UserProfile />
         </div>
     )
