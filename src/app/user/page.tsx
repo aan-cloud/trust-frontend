2534dc92241e24fba75a79e939e68fe9d7af0297
aan-cloud <evirsanti@gmail.com>
2025-02-01 22:06:36 +0800

    refactor: change folder name

--- src/app/user/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UserProfile } from "@/components/pages/server/profile"
-
-export default function User () {
-    return (
-        <div className="sm:px-24 px-4 py-5 bg-white flex flex-col gap-3 pt-5 w-full">
-            <UserProfile />
-        </div>
-    )
-}
