a3b60214c84b7d6ed4b82e31953ffebaaf81fb76
aan-cloud <evirsanti@gmail.com>
2025-02-02 07:10:16 +0800

    fix: ui button width fit

--- src/components/ui/userCard.tsx
+++ src/components/ui/userCard.tsx
@@ -37,7 +37,7 @@ export const UserCart: React.FC<ModalProps> = ({ isOpen, ref, handleNavLinkClick
       <CardContent className="p-4">
         <nav className="flex flex-col space-y-2">
           <Link className="justify-start pl-1 font-semibold font-poppins" href={"/profile"} onClick={handleNavLinkClick}>
-          <Button variant="ghost" className="justify-star pl-1 font-semibold font-poppins ">
+          <Button variant="ghost" className="w-full justify-start pl-1 font-semibold font-poppins ">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-12">
                 <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
             </svg>
@@ -45,14 +45,14 @@ export const UserCart: React.FC<ModalProps> = ({ isOpen, ref, handleNavLinkClick
           </Button>
           </Link >
           <Link  href={"/"}>
-          <Button variant="ghost" className="justify-start pl-1 font-semibold font-poppins ">
+          <Button variant="ghost" className="w-full justify-start pl-1 font-semibold font-poppins ">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-12">
                 <path fillRule="evenodd" d="M1.5 7.125c0-1.036.84-1.875 1.875-1.875h6c1.036 0 1.875.84 1.875 1.875v3.75c0 1.036-.84 1.875-1.875 1.875h-6A1.875 1.875 0 0 1 1.5 10.875v-3.75Zm12 1.5c0-1.036.84-1.875 1.875-1.875h5.25c1.035 0 1.875.84 1.875 1.875v8.25c0 1.035-.84 1.875-1.875 1.875h-5.25a1.875 1.875 0 0 1-1.875-1.875v-8.25ZM3 16.125c0-1.036.84-1.875 1.875-1.875h5.25c1.036 0 1.875.84 1.875 1.875v2.25c0 1.035-.84 1.875-1.875 1.875h-5.25A1.875 1.875 0 0 1 3 18.375v-2.25Z" clipRule="evenodd" />
             </svg>
             Dashboard
           </Button>
           </Link >
-          <Link href={"/"} className="justify-start pl-1 font-semibold font-poppins ">
+          <Link href={"/"} className="w-full justify-start pl-1 font-semibold font-poppins ">
           <Button variant="ghost" className="justify-start pl-1 font-semibold font-poppins ">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-12">
                 <path fillRule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
