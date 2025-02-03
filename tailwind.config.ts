0cb07743e935e06f281c9c6b4b5a9ecadb415447
aan-cloud <evirsanti@gmail.com>
2025-02-03 23:52:05 +0800

    chore: light color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,9 @@ export default {
   				DEFAULT: 'hsl(var(--card))',
   				foreground: 'hsl(var(--card-foreground))'
   			},
+			light: {
+				DEFAULT: 'hsl(var(--light))'
+			},
   			popover: {
   				DEFAULT: 'hsl(var(--popover))',
   				foreground: 'hsl(var(--popover-foreground))'
