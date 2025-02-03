4bf31e4a35fa2a9fe220aef0d07f29adcfe65708
aan-cloud <evirsanti@gmail.com>
2025-02-03 23:58:10 +0800

    chore: add user id state to managing the url route

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -5,15 +5,18 @@ import React, { createContext, useContext, useState, ReactNode } from "react";
 interface AuthContextType {
     userName: string | null;
     setUserName: (value: string | null) => void;
+    userId: string | null;
+    setUserId: (value: string | null) => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
     const [userName, setUserName] = useState<string | null>("");
+    const [userId, setUserId] = useState<string | null>("");
 
     return (
-      <AuthContext.Provider value={{ userName, setUserName}}>
+      <AuthContext.Provider value={{ userName, setUserName, userId, setUserId}}>
         {children}
       </AuthContext.Provider>
     );
