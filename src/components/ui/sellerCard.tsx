dd8a9b6ee6b55b713c55b516cb7f941ff6e6dec1
aan-cloud <evirsanti@gmail.com>
2025-02-04 12:41:57 +0800

    feat: seller card component

---
+++ src/components/ui/sellerCard.tsx
@@ -0,0 +1,33 @@
+import { sellerSchema } from "@/schema/seller"
+import Image from "next/image"
+import { z } from "zod"
+import { Button } from "./button"
+
+type SellerSchema = z.infer<typeof sellerSchema>
+
+export const SellerCard = ({ seller }: { seller: SellerSchema}) => {
+    return (
+        <div className="flex flex-row flex-grow sm:min-w-[45%] shadow-md bg-light">
+            <div>
+                <Image 
+                    src={seller.avatarUrl}
+                    alt={seller.userName}
+                    width={200}
+                    height={200}
+                />
+            </div>
+            <div className="px-4 py-2 flex flex-col gap-3 flex-grow">
+                <h1 className="font-sans text-xl font-bold">{seller.userName}</h1>
+                <small className="font-poopins text-gray-700">{seller.description}</small>
+                <div className="flex-grow flex justify-between items-end w-full gap-3">
+                    <Button>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
+                        </svg>
+                    </Button>
+                    <Button className="flex-grow">Products</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
