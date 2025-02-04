b82ccd9756a54dccb2215d8ea1a8b0fd84a46fd9
aan-cloud <evirsanti@gmail.com>
2025-02-04 12:42:10 +0800

    chore: get all seller request

--- src/requests/seller.ts
+++ src/requests/seller.ts
@@ -1,8 +1,36 @@
 import 'dotenv/config';
 import { allProductInDashboard } from '@/schema/product';
 import { z } from 'zod';
+import { sellerSchema } from '@/schema/seller';
 
 type ProductSchema = z.infer<typeof allProductInDashboard>
+type SellerSchema = z.infer<typeof sellerSchema>[]
+
+export const getAllSeler = async () => {
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/seller`;
+
+    try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const sellers: SellerSchema = await response.json();
+
+      return sellers;
+      
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: Error | any) {
+      console.log(error.message);
+    }
+
+}
 
 export const getSellerDashBoard = async (sellerId: string, token: string) => {
     const url = `${process.env.NEXT_PUBLIC_API_URL}/seller/${sellerId}/dashboard`;
